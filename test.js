cd090ab8bacebb76e33b244d2026ecebdeb72406
madpul24 <madul2411@gmail.com>
2021-08-11 21:34:28 +0700

    commit to main

---
+++ test.js
@@ -0,0 +1,10 @@
+//fungsi menghitung luas dengan menggunakan function declaration
+
+//segi empat (sisi * sisi)
+//segi panjang (panjang * lebar)
+//segi tiga (0.5 * alas * tinggi)
+//lingkaran (3.14 * jari-jari * 2)
+
+//fungsi menghitung
+
+
